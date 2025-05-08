9d088ae6051b193d218700c47498dcc4e219efc4
Zelalem Belay <Zelalem.belay@aa.com>
2025-05-08 08:02:01 -0500

    add workers

---
+++ adey-security/src/test/java/com/adeytech/adey_security/AdeySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adeytech.adey_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdeySecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
