03858a2ce3125ecd5a54138e08c8f0cf1d78c21a
Zelalem Belay <zelalem.belay1@gmail.com>
2025-05-08 13:45:49 -0500

    Update CorsConfig.java

--- adey-security/src/main/java/com/adeytech/adey_security/CorsConfig.java
+++ adey-security/src/main/java/com/adeytech/adey_security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:8080", "http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "FETCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
