4ddef259679a8fa208dafa0dda3f187c1dae6fbd
Zelalem Belay <Zelalem.belay@aa.com>
2025-05-08 07:51:30 -0500

    add workers

---
+++ adey-security/src/main/java/com/adeytech/adey_security/AdeySecurityApplication.java
@@ -0,0 +1,13 @@
+package com.adeytech.adey_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdeySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdeySecurityApplication.class, args);
+	}
+
+}
